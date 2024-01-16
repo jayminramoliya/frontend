80cbe85db69f07ecd3fcedb5c0f618075af816fb
Jenil Ramoliya <jayminramoliya4380@gmail.com>
2024-01-17 00:48:57 +0530

    data

--- JavaScript/JavaScript_Task/02Result/script.js
+++ JavaScript/JavaScript_Task/02Result/script.js
@@ -3,8 +3,9 @@ function create(){
     let sub2 = parseInt(document.getElementById("sub2").value)
     let sub3 = parseInt(document.getElementById("sub3").value)
     let sub4 = parseInt(document.getElementById("sub4").value)
-    let total = sub1 + sub2 + sub3 + sub4
-    let avarage = parseFloat(total / 4)
+    let sub5 = parseInt(document.getElementById("sub5").value)
+    let total = sub1 + sub2 + sub3 + sub4 + sub5
+    let avarage = parseFloat(total / 5)
     document.getElementById("total").innerHTML = "Total : " + total;
     document.getElementById("avarage").innerHTML = "avarage : " + avarage;
 }
